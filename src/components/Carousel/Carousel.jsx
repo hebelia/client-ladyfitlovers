f0941d791b9ecfa28adb2f6c54be834d9ce5f0a4
hebelia <heberomeu@gmail.com>
2023-10-29 03:44:57 -0300

    Readme.md creation

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -15,7 +15,7 @@ const Carrousel = () => {
         <img className='carouselSlide' src='https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697137147/2_pfqrjd.png' />
       </div>
       <div>
-        <img className='carouselSlide' src='https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697137147/3_eqcnep.png' />
+        <img className='carouselSlide' src='/img/Logo-slide.png' />
       </div>
     </Carousel>
   );
